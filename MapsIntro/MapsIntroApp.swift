791fed2ab1c4a06f1e608559b10c6aaf81523d7b
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-06-08 12:06:31 +0200

    Initial Commit

---
+++ MapsIntro/MapsIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsIntroApp.swift
+//  MapsIntro
+//
+//  Created by Philip Andersson on 2023-06-08.
+//
+
+import SwiftUI
+
+@main
+struct MapsIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
